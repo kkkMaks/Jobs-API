83b1497e5eec97e3c7e17d247b67ed086c526f99
kkkMaks <kurchenko.maks.kpi@gmail.com>
2023-05-23 00:22:35 +0300

    swagger docs added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
-
 const connectDB = require("./db/connect");
-
 const authenticateUser = require("./middleware/auth");
-
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
@@ -15,7 +23,22 @@ const jobsRouter = require("./routes/jobs");
 
 const app = express();
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send("<h1> Job API </h1> <a href='/api-docs'> Documentation </a>");
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
